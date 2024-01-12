26e2c1ce5e9a14d92a142c4ea37258065239888b
atocf <atocf@hotmail.com>
2024-01-12 15:36:52 -0300

    Configurando Mysql

--- src/main/java/br/com/atocf/pedidos/PedidosApplication.java
+++ src/main/java/br/com/atocf/pedidos/PedidosApplication.java
@@ -2,7 +2,11 @@ package br.com.atocf.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport
+@EnableCaching
 @SpringBootApplication
 public class PedidosApplication {
 
--- src/test/java/br/com/atocf/pedidos/PedidosApplicationTests.java
+++ src/test/java/br/com/atocf/pedidos/PedidosApplicationTests.java
@@ -8,6 +8,6 @@ class PedidosApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
-
 }
