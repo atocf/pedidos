881c8e1d2c37e17195117c09837595d249532a7d
atocf <atocf@hotmail.com>
2024-01-12 23:09:48 -0300

    Testes

--- src/test/java/br/com/atocf/pedidos/repository/ClienteRepositoryTests.java
+++ src/test/java/br/com/atocf/pedidos/repository/ClienteRepositoryTests.java
@@ -1,9 +1,6 @@
 package br.com.atocf.pedidos.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,32 +14,28 @@ import br.com.atocf.pedidos.entity.Cliente;
 @DataJpaTest
 @DisplayName("Teste para o repositório de clientes.")
 public class ClienteRepositoryTests {
-	
+
 	@Autowired
 	private ClienteRepository repository;
-	
+
 	private static Cliente cliente1;
-	private static Optional<Cliente> dcliente1;
-	
+
 	@BeforeEach
 	public void setup() {
 		MockitoAnnotations.openMocks(this);
-		
-		cliente1 = new Cliente("TesteCliente1");
 
-		dcliente1 = Optional.of(cliente1);
-		dcliente1.get().setId(1);
+		cliente1 = new Cliente("TesteCliente1");
 	}
-	
+
 	@Test
 	@DisplayName("Buscar cliente por nome com sucesso.")
 	void findByName_ComSucesso() {
-		when(repository.save(cliente1)).thenReturn(dcliente1.get());
-		
+		Cliente clienteSalvada = repository.save(cliente1);
+
 		Cliente clienteFind = repository.findByName(cliente1.getName()).get();
 
 		assertThat(clienteFind).isNotNull();
-		
-		assertThat(clienteFind.getName()).isEqualTo(dcliente1.get().getName());
+
+		assertThat(clienteFind.getName()).isEqualTo(clienteSalvada.getName());
 	}
 }
